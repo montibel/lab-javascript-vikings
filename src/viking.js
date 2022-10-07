0a285a53eb8123ff8de0b6f4509bea5c48324208
“Marcela <mpmontiela@gmail.com>
2022-10-07 11:27:04 +0200

    done

--- src/viking.js
+++ src/viking.js
@@ -1,7 +1,18 @@
 // Soldier
 class Soldier {
-    constructor(health, strength) {}
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+attack () {
+ return this.strength;
 }
+receiveDamage(damage) {
+    this.health -= damage;
+ }
+}
+    
+
 
 // Viking
 class Viking {}
